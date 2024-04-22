82c810742c4ceada2c27601b73d9f06a27ae274a
Gireesh Galla <161557278+gireeshgalla@users.noreply.github.com>
2024-04-22 20:50:02 +0530

    Add files via upload

---
+++ datatyypes.py
@@ -0,0 +1,44 @@
+'''
+int(1,-1)
+boolean
+float
+complex
+'''
+#To know which datatype
+#a=1
+#b=-1
+#print(type(a),type(b))
+
+#a=True
+#b=False
+#print(type(a),type(b))
+
+
+
+#print(True==1)
+#print(False==0)
+
+
+#c=4.44
+#print(type(c))
+
+#v=2+5j
+#print(type(v))
+
+'''
+int()
+float()     #Datatype convert from 1 to 1
+bool()
+str()
+'''
+#a=2
+#print(float(a))
+
+b=5.5
+print(int(b))
+
+
+'''
+data lose--> explicit type conversion
+no data lose-->implict type conversion
+'''
