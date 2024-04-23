f30ed822b190e014a6cfab1fa3fe0e3443bb58fa
Gireesh Galla <161557278+gireeshgalla@users.noreply.github.com>
2024-04-23 17:24:44 +0530

    Add files via upload

---
+++ jumping.py
@@ -0,0 +1,27 @@
+                                  #Jumping Statements
+
+
+'''
+pass
+break
+continue
+'''
+#if True:
+#    pass
+
+#for i in "pythonlife":      #Break
+#    if i=='l':
+#        break
+#    print(i)
+
+#for i in "pythonlife":
+#    if i=='l':              #Continue
+#        continue
+#    print(i)
+
+'''
+pass--Ignore
+Break--Terminate
+continue--Skip(Current iteration)
+
+'''
