1409658bef40665901bbda4235781875b899ecf7
Gireesh Galla <161557278+gireeshgalla@users.noreply.github.com>
2024-04-23 17:11:21 +0530

    Add files via upload

---
+++ Looping.py
@@ -0,0 +1,32 @@
+'''
+for
+while
+nested loop
+'''
+#for i in range(1,10,2):
+# print(i)
+
+
+#a=[4,4,4,4,4,35,2335,3,55]
+#for i in a:                 #List
+#    print(i)
+
+
+#a='python life'
+#for i in a:            #String  
+#    print(i)
+
+
+#while True:
+#    print("hiiii")
+
+#kiran=9
+#while kiran<15:
+#    print("hii",kiran)
+#    kiran+=1
+
+
+
+for i in range(0,10):
+    for j in range(0,100):
+        print(i+j)
