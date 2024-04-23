b66333a347e18843aa69c823b0a0e5bccc2a98b0
Gireesh Galla <161557278+gireeshgalla@users.noreply.github.com>
2024-04-23 16:37:27 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,48 @@
+'''
+Desicion making statements
+
+
+if
+else
+elif
+nested if
+'''
+
+'''
+syntax:
+if condition:
+    statements
+else:
+    stayements
+'''
+
+#if True:
+#    print("nenu if")
+#elif True:
+#    print("nenu elif")
+#else:
+#    print("nenu else")
+
+
+
+
+
+
+#if True:
+#    print("outer if")
+#    if True:
+#        print("inner if")
+#    else:
+#        print("inner elsse")
+#else:
+#    print("outer else")  
+
+
+
+#age=18
+#if age>18:   # if age>18 or age ==18
+#    print("you can vote")
+#elif age==18:
+#    print("you can vote")
+#else:
+#    print("wait")
