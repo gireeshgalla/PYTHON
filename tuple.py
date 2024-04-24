80ed1fe00a5b0f4b10eb8e0e984ca2ed5acfdb42
Gireesh Galla <161557278+gireeshgalla@users.noreply.github.com>
2024-04-24 19:12:19 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,27 @@
+#c=()
+#print(type(c))
+
+c=(1,24,1,3,14,14,)
+#print(c[0:4:2])
+#print(min(c))
+#print(max(c))
+#print(sum(c))
+#print(len(c))
+
+t1=(1,2,3)
+t2= (4,5,6)        #Concadination
+print(t1+t2)
+
+print(c*11)          #Repetation
+
+
+for i in c:
+    print(i)         #Iteration
+
+
+print(11 not c)
+print(1 in c)
+                       #Membership & Identity
+t1=(1,2,3)
+t2=(4,5,6)
+print(t1 is t2)
