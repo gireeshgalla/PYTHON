c9207bb39a496d3724867451a23c19d07ee29883
Gireesh Galla <161557278+gireeshgalla@users.noreply.github.com>
2024-04-30 20:36:09 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,24 @@
+#d={}
+#print(type(d))
+
+d={1:'abc',22:'kiran','pythonlife':1}
+#print(d[pythonlife])
+
+'''
+get
+update
+values
+keys
+items
+'''
+
+#print(d.get(1))
+#print(d.keys())
+#print(d.values())
+#print(d.items())
+#d.update({1111:2222})
+#print(d)
+
+
+for i,j in{1:'abc',22:'kiran','pythonlife':1}.items():
+    print(i,j)
