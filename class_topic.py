bb5ddb868ffd59b40f7afc22fde5a58485a578a5
Gireesh Galla <161557278+gireeshgalla@users.noreply.github.com>
2024-05-16 11:33:31 +0530

    Add files via upload

---
+++ class_topic.py
@@ -0,0 +1,16 @@
+'''
+Blue print of a object
+'''
+
+#class pythonlife():
+#   a=3
+#    print(a)
+
+class pythonlife():
+    a=2
+    def output(self):
+        print(self.a)
+b=pythonlife()
+v=pythonlife()
+b.output()    
+v.output()    
